e0a5b0137e63cf5b25d33ad4577be3fefc4f857b
HenryHolzkamp <henry.holzkamp@tu-braunschweig.de>
2025-06-23 10:12:41 +0200

    Updated python_mpi_test.py

--- python/python_mpi_test.py
+++ python/python_mpi_test.py
@@ -5,15 +5,21 @@ import numpy as np
 comm = MPI.COMM_WORLD
 
 # Get the number of processes
-world_size = comm.Get_size()
+MPI_size = comm.Get_size()
 
 # Get the rank of the process
-world_rank = comm.Get_rank()
+MPI_rank = comm.Get_rank()
 
-if world_rank == 0:
-    number = np.random.rand()
-    comm.send(number, dest=1, tag=0)
-    print(f"Process {world_rank} sent {number} to process 1.")
-else:
-    recv_number = comm.recv(source=0, tag=0)
-    print(f"Process 1 got {recv_number}.")
+# ... initialize MPI ... (slide 11) #
+nr_rand_ints = 1000
+rand_ints = np.random.randint(0, 101, nr_rand_ints)
+local_sum = np.sum(rand_ints)
+sendbuf = np.array([local_sum], dtype = 'i')
+
+recvbuf = np.empty(1, dtype = 'i')
+comm.Reduce(sendbuf, recvbuf, op=MPI.SUM, root=0)
+
+if MPI_rank == 0:
+    global_sum = recvbuf[0]
+    global_average = global_sum / (MPI_size * nr_rand_ints)
+    print(f"Global average: {global_average}")
